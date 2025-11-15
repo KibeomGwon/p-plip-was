695e18a1fda3af1f1afb7b7cf1c775f2d40cea45
KibeomGwon <xtcc02332@naver.com>
2025-11-15 19:46:44 +0900

    Chore : 프로젝트 초기화 프로젝트 초기화 의존성들 추가 ( swagger 까지 )

---
+++ src/main/java/review/pplip/PplipApplication.java
@@ -0,0 +1,13 @@
+package review.pplip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PplipApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PplipApplication.class, args);
+    }
+
+}
---
+++ src/test/java/review/pplip/PplipApplicationTests.java
@@ -0,0 +1,13 @@
+package review.pplip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PplipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
