088d215eb4f596ea3696f7c756da79f648d2fbc5
성민제 <minj21@naver.com>
2025-11-18 10:17:30 +0900

    Chore : application.yml추가 application-sample.yml 추가 .gitignore에 application.yml추가 패키지 review.pplip에서 com.pplip으로 변경

--- src/main/java/com/pplip/PplipApplication.java
+++ src/main/java/com/pplip/PplipApplication.java
@@ -1,4 +1,4 @@
-package review.pplip;
+package com.pplip;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/pplip/PplipApplicationTests.java
+++ src/test/java/com/pplip/PplipApplicationTests.java
@@ -1,4 +1,4 @@
-package review.pplip;
+package com.pplip;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
