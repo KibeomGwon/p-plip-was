4408f55f77d5e51508c90bd04daf108d9faf6fe7
권기범 <xtcc02332@naver.com>
2025-11-21 16:58:23 +0900

    Add: Entity들 추가 DB의 테이블과 맵핑될 entity들 추가

---
+++ src/main/java/com/pplip/domain/file/persistence/entity/FileProperty.java
@@ -0,0 +1,22 @@
+package com.pplip.domain.file.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public abstract class FileProperty {
+    private Long id;
+    private String originFileName;
+    private String path;
+    private String savedFileName;
+    private long size;
+
+    private LocalDateTime updatedAt;
+
+    abstract ImageType supports();
+}
