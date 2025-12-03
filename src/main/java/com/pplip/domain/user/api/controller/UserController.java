3c3e247e0a7be08a8bdebc9f185a26094f5d53fb
KibeomGwon <xtcc02332@naver.com>
2025-12-03 15:31:19 +0900

    Feat: Email 기능 구현 email 전송 기능 구현 email 인증 기능 구현 email 기능 엔드포인트 구현

--- src/main/java/com/pplip/domain/user/api/controller/UserController.java
+++ src/main/java/com/pplip/domain/user/api/controller/UserController.java
@@ -2,9 +2,11 @@ package com.pplip.domain.user.api.controller;
 
 import com.pplip.domain.user.api.request.UserRequest;
 import com.pplip.domain.user.api.response.UserResponse;
+import com.pplip.domain.user.persistence.service.EmailService;
 import com.pplip.global.api.code.SuccessCode;
 import com.pplip.global.api.response.CommonResponse;
 import com.pplip.global.docs.UserDocsController;
+import jakarta.mail.MessagingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController implements UserDocsController {
+
+    private final EmailService emailService;
 
     /**
      * 닉네임 중복 여부를 확인합니다.
@@ -48,19 +52,22 @@ public class UserController implements UserDocsController {
      */
     @Override
     @PostMapping("/join/send-verification-email")
-    public CommonResponse<Void> sendVerificationEmail(@RequestBody UserRequest.Email email) {
+    public CommonResponse<Void> sendVerificationEmail(@RequestBody UserRequest.Email email) throws MessagingException {
+        emailService.sendEmail(email);
         return CommonResponse.success(SuccessCode.CREATED, null);
     }
 
     /**
      * 이메일 인증 코드를 확인합니다.
      *
-     * @param email 확인할 이메일 및 인증 코드
+     * @param emailCheck 확인할 이메일 및 인증 코드
      * @return 이메일 인증 확인 결과
      */
     @Override
-    @GetMapping("/join/verification-email")
-    public CommonResponse<UserResponse.EmailCheck> verificationEmail(@RequestBody UserRequest.Email email) {
+    @PostMapping("/join/verification-email")
+    public CommonResponse<UserResponse.EmailCheck> verificationEmail(@RequestBody UserRequest.EmailCheck emailCheck) {
+        emailService.validate(emailCheck);
+
         return CommonResponse.success(SuccessCode.SUCCESS, null);
     }
 
