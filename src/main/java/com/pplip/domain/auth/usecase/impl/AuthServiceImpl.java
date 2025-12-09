719a3e714f4bb7e6d27a0038bf0cbd3735e13b04
smj1513 <minj21@naver.com>
2025-12-09 22:04:00 +0900

    add: 토큰 타입 추가 (access, refresh)

--- src/main/java/com/pplip/domain/auth/usecase/impl/AuthServiceImpl.java
+++ src/main/java/com/pplip/domain/auth/usecase/impl/AuthServiceImpl.java
@@ -2,8 +2,10 @@ package com.pplip.domain.auth.usecase.impl;
 
 import com.pplip.domain.auth.jwt.Jwt;
 import com.pplip.domain.auth.jwt.JwtUtil;
+import com.pplip.domain.auth.persistence.entity.Account;
 import com.pplip.domain.auth.usecase.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,7 +18,8 @@ public class AuthServiceImpl implements AuthService {
 
 	@Override
 	public Jwt issueNewTokens(String refreshToken) {
-
-		return null;
+		Account account = jwtUtil.resolveRefreshToken(refreshToken);
+		UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(account.getUserId(), null, account.getAuthorities());
+		return jwtUtil.generate(authToken);
 	}
 }
