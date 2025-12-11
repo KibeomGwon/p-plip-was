8ce9aba14cf1790952987dd0e652b4c1e9053ae1
smj1513 <minj21@naver.com>
2025-12-11 17:47:07 +0900

    feat: 리프래시 토큰 재발급 수정

--- src/main/java/com/pplip/global/docs/AuthDocsController.java
+++ src/main/java/com/pplip/global/docs/AuthDocsController.java
@@ -7,14 +7,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.CookieValue;
+
+import java.util.Map;
 
 @Tag(name = "인증/인가 API", description = "인증/인가 API")
 public interface AuthDocsController {
 
     @Operation(summary = "토큰 재발급", description = "리프레시 토큰을 통해 JWT 토큰을 재발급받습니다.(브라우저 정책상 작동은 하지않음)")
     @ApiResponse(responseCode = "201", description = "생성")
-    CommonResponse<Jwt> refreshAccessToken(String refreshToken);
+    CommonResponse<Map<String, String>> refreshAccessToken(String refreshToken, HttpServletResponse response);
 
     @Operation(summary = "로그인", description = "ID/PW를 통해 JWT 토큰을 발급받습니다.")
     @ApiResponse(responseCode = "200", description = "성공. 응답 헤더의 Authorization, refreshToken 필드 확인")
